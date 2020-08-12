4229129c8f4c763db0112e19b68196ab0e879f7b
Aaron Stoiano <aaronstoiano@gmail.com>
2020-08-11 23:36:08 -0400

    final commit. Put finishing touches on JS. Fixed my save button

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,38 +1,43 @@
-$(document).ready(function () {
-
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-    $("#hour").text(moment().format("HH"));
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+$("#hour").text(moment().format("HH"));
 
 
-    $(".saveBtn").on("click", function () {
-        console.log(this);
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+$(".saveBtn").on("click", save);
 
-        localStorage.setItem(time, text);
+function displayLocal() {
+    $(".description").each(function () {
+        var blockD = $(this).parent().attr("id");
+        $(this).val(localStorage.getItem(blockD));
+    }
+
+    )
+}
+function update() {
+    var currentHour = moment().format("HH");
+    $(".hour-block").each(function () {
+        var blockHour = $(this).attr("id")
+
+        if (currentHour > blockHour) {
+            $(this).addClass("past")
+        } else if (currentHour = blockHour) {
+            $(this).addClass("present")
+        } else {
+            $(this).addClass("future")
+        }
+
     })
+}
 
-    function update() {
-        var currentHour = moment().format("HH");
-        $(".hour-block").each(function(){
-            var blockHour = $(this).attr("id")
+update();
 
-            if (currentHour>blockHour) {
-                $(this).addClass("past")
-            } else if (currentHour=blockHour) {
-                $(this).addClass("present")
-            } else {
-                $(this).addClass("future")
-            }
-
-        })
-    }
-
-    update();
-
-    function save() {
-        var description = $(this)
-    }
+function save() {
+    var description = $(this).siblings(".description").val();
+    var key = $(this).parent(attr)("id");
+    var hour = $(this).siblings().text();
+    var today = moment().format("MMM Do YYYY");
+    localStorage.setItem(key, description)
+    alert("Saved: " + "'" + description + "' " + "for " + hour + ", " + today)
+}
 
 
 
@@ -45,7 +50,3 @@ $(document).ready(function () {
 
 
 
-
-
-
-})
